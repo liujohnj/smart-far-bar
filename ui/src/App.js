6d1124b287e4626b74e1a0dd3d78f19240597305
liujohnj <48953359+liujohnj@users.noreply.github.com>
2021-10-14 11:48:10 -0400

    added material-ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,22 @@
-
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello.
-        </h1>
-      </header>
-    </div>
+    <>
+        <Typography variant="h1">
+            Hello
+        </Typography>
+
+        <Button variant="contained">
+            Click me
+        </Button>
+
+    </>
   );
 }
 
