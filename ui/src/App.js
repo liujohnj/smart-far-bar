934d6cb1b3a3df71ba8c87865abaec79281af02b
liujohnj <48953359+liujohnj@users.noreply.github.com>
2021-10-14 13:53:54 -0400

    fixed/added test choices

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,20 +2,13 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+
+import TestWorkflow from './components/TestWorkflow';
 
 const App = () => {
   return (
     <>
-        <Typography variant="h1">
-            Hello
-        </Typography>
-
-        <Button variant="contained">
-            Click me
-        </Button>
-
+        <TestWorkflow />
     </>
   );
 }
